f3405db253cdb97213718d750dd87f761b287298
Bryson Seiler <Bryson.Seiler@gmail.com>
2018-06-07 16:32:24 -0600

    Initial commit

---
+++ walkthrough.py
@@ -0,0 +1,17 @@
+"""
+This script is a walked-through example for 
+the process of Latent Semantic Analysis.
+
+@author: Bryson Seiler
+"""
+
+
+#Here are the documents that we will be using:
+corpus = ['Cats are good pets. They are clean and not noisy. Dogs are not as clean as cats, but are still great pets and companions.',
+          'Cats and dogs have always been great household pets. While cats prefer to be inside. Dogs love to go on walks.',
+          'Italy is my favorite country. There is so much to see, so much to do. If you are looking to travel to a different country, you should travel to Italy.',
+          'There are 195 countries in the world today, many of which you should travel to. In particular, Italy is an excellent travel destination with lots to see.']
+    
+
+
+
